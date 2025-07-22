70546a29020b357204c82d21703c964c4e4e2663
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-22 11:27:10 +0530

    Create While.java

---
+++ While.java
@@ -0,0 +1,14 @@
+//While loop program
+import java.lang.*;
+class While
+{
+    public static void main(String [] args)
+    {
+        int Num=1;
+        while(Num<=10)
+        {
+            System.out.print("\n"+(Num));
+            Num++;
+        }
+    }
+}
