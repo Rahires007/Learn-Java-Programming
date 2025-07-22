c52661a1e57fe0b9db3c0aed060cd90c23a21d59
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-22 12:21:27 +0530

    Create Constructor.java

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+//Constructor Program
+import java.lang.*;
+import java.util.*;
+class Sample
+{
+    Sample()
+    {
+      System.out.print("\nHello World...");
+    }
+    public static void main(String [] args)
+    {
+        Sample S=new Sample();
+    }
+}
