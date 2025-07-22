fe86aa1693f1312bbccba91aaef785c09d337cf1
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-22 11:30:57 +0530

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+//Do While loop program
+import java.lang.*;
+class DoWhile
+{
+    public static void main(String [] args)
+    {
+        int Num=1;
+        do
+        {
+            System.out.print("\n"+(Num));
+            Num++;
+        }
+        while(Num<=10);
+    }
+}
