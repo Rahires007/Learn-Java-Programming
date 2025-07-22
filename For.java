abfbb53fcd2f132c9ededb9b2c3aa3cb441d6dce
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-22 11:29:08 +0530

    Create For.java

---
+++ For.java
@@ -0,0 +1,13 @@
+//For loop program 
+import java.lang.*;
+class For
+{
+    public static void main(String [] args)
+    {
+        int Num;
+        for(Num=1; Num<=10; Num++)
+        {
+            System.out.print("\n"+(Num));
+        }
+    }
+}
